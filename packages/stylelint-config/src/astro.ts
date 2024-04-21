fec316b291ba5dce53059d1c12d7e8f33a4220e8
sushi-chaaaan <mail@sushichan.live>
2024-04-21 20:06:43 +0900

    fix: stylelint module

--- packages/stylelint-config/src/astro.ts
+++ packages/stylelint-config/src/astro.ts
@@ -3,9 +3,20 @@ import type { Config } from "stylelint"
 import { baseConfig } from "./base"
 import { astroExtend } from "./extend"
 
+const { rules: baseRules, ...base } = baseConfig
+
 const astroConfig: Config = {
-  ...baseConfig,
+  ...base,
   extends: astroExtend,
+  rules: {
+    ...baseRules,
+    "at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: ["tailwind"],
+      },
+    ],
+  },
 }
 
 export default astroConfig
--- packages/stylelint-config/src/scss.ts
+++ packages/stylelint-config/src/scss.ts
@@ -3,9 +3,26 @@ import type { Config } from "stylelint"
 import { baseConfig } from "./base"
 import { scssExtend } from "./extend"
 
+const { rules: baseRules, ...base } = baseConfig
+
 const scssConfig: Config = {
-  ...baseConfig,
+  ...base,
   extends: scssExtend,
+  rules: {
+    ...baseRules,
+    "at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: ["tailwind"],
+      },
+    ],
+    "scss/at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: ["tailwind"],
+      },
+    ],
+  },
 }
 
 export default scssConfig
