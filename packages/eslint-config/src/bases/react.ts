7926aa7f42f89940996c8dee33eacc6d973b81e0
Kentaro Suzuki <mail@sushichan.live>
2024-04-23 01:39:31 +0900

    fix: eslint build config (#41)
    * fix: eslint build config
    
    * chore: changeset

--- packages/eslint-config/src/bases/react.ts
+++ packages/eslint-config/src/bases/react.ts
@@ -2,15 +2,17 @@ import type { Linter } from "eslint"
 
 //@ts-expect-error no types
 import react from "eslint-plugin-react"
+//@ts-expect-error no types
+import reactRecommended from "eslint-plugin-react/configs/recommended"
 import globals from "globals"
 
 import { compat } from "../lib/compat"
-
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 const reactConfig: Linter.FlatConfig[] = [
   ...compat.extends("plugin:react-hooks/recommended"),
   {
     files: ["**/*.{js,cjs,mjs,ts,cts,mts,jsx,tsx}"],
+    ...reactRecommended,
     languageOptions: {
       globals: {
         ...globals.browser,
--- packages/eslint-config/src/index.ts
+++ packages/eslint-config/src/index.ts
@@ -1,1 +1,4 @@
-export { compat } from "./lib/compat"
+export { default as astro } from "./astro"
+export { default as js } from "./js"
+export { default as react } from "./react"
+export { default as ts } from "./ts"
