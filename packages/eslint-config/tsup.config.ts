91b46d4143fa2463eff7b57f9b7bacdc66fd61f2
sushi-chaaaan <mail@sushichan.live>
2024-04-23 00:37:32 +0900

    fix: export as esm format

--- packages/eslint-config/tsup.config.ts
+++ packages/eslint-config/tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   clean: true, // build前にディレクトリ内を削除するか
   dts: true,
   entry: ["./src/*.ts", "./src/addons/*.ts"], // バンドルするファイルを指定
-  format: ["cjs"], // 出力する形式を指定
+  format: ["esm"], // 出力する形式を指定
   minify: process.env.NODE_ENV === "production",
   outDir: "dist",
   sourcemap: false, // soucemapを出力するか
