28e1f981ad9bc4a7c370e7cd831d0ccca356c878
sushi-chaaaan <mail@sushichan.live>
2024-04-23 00:46:38 +0900

    fix: build stylelint config

--- packages/stylelint-config/tsup.config.ts
+++ packages/stylelint-config/tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   clean: true, // build前にディレクトリ内を削除するか
   dts: true,
   entry: ["./src/*.ts"], // バンドルするファイルを指定
-  format: ["cjs", "esm"], // 出力する形式を指定
+  format: ["esm"], // 出力する形式を指定
   minify: process.env.NODE_ENV === "production",
   outDir: "dist",
   sourcemap: false, // soucemapを出力するか
