c82270dd72a89366fb071dff2edb3f04f0775528
Kentaro Suzuki <mail@sushichan.live>
2024-04-23 02:42:50 +0900

    fix/prettier stylelint build config (#51)
    * fix: prettier build config
    
    * fix: prettier build config
    
    * chore: changeset

---
+++ .changeset/smooth-spoons-design.md
@@ -0,0 +1,6 @@
+---
+"@virtual-live-lab/stylelint-config": patch
+"@virtual-live-lab/prettier-config": patch
+---
+
+fix build config
--- packages/stylelint-config/tsup.config.ts
+++ packages/stylelint-config/tsup.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "tsup"
 
 export default defineConfig({
   clean: true, // build前にディレクトリ内を削除するか
-  dts: true,
   entry: ["./src/*.ts"], // バンドルするファイルを指定
+  experimentalDts: true,
   format: ["esm"], // 出力する形式を指定
   minify: process.env.NODE_ENV === "production",
   outDir: "dist",
