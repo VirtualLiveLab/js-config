ee21287b849a67503a0cd4587a28672fb39292ae
Kentaro Suzuki <mail@sushichan.live>
2024-04-23 00:48:13 +0900

    fix/deps (#39)
    * fix: dependencies
    
    * fix: mark dev dependencies
    
    * fix: export as esm format
    
    * fix: pretteir build config
    
    * fix: build stylelint config
    
    * chore: changeset

--- packages/prettier-config/tsup.config.ts
+++ packages/prettier-config/tsup.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from "tsup"
 export default defineConfig({
   clean: true, // build前にディレクトリ内を削除するか
   dts: true,
-  entry: ["./index.ts", "./astro.ts"], // バンドルするファイルを指定
-  format: ["cjs", "esm"], // 出力する形式を指定
+  entry: ["./src/*.ts"], // バンドルするファイルを指定
+  format: ["esm"], // 出力する形式を指定
   minify: process.env.NODE_ENV === "production",
+  outDir: "dist",
   sourcemap: false, // soucemapを出力するか
   splitting: false, // バンドルしないで分割するか
   treeshake: true,
