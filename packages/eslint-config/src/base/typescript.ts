f11e15fd272f21a3d24b9b16b6b3fbb0a3abd8cf
Kentaro Suzuki <mail@sushichan.live>
2024-10-23 12:19:17 +0900

    typesciript-eslintの有効化ルールを増やす (#274)
    * add some tseslint rules
    
    * chore: update snapshot (github-actions)
    
    ---------
    
    Co-authored-by: actions-miku[bot] <185786754+actions-miku[bot]@users.noreply.github.com>

--- packages/eslint-config/src/base/typescript.ts
+++ packages/eslint-config/src/base/typescript.ts
@@ -24,8 +24,6 @@ const tsConfig = tseslint.config({
     "@stylistic/ts": stylisticTs,
   },
   rules: {
-    // SEE: https://zenn.dev/cybozu_frontend/articles/ts-eslint-v6-guide
-    // v6 で recommended から削除されたものを有効化
     "@stylistic/ts/no-extra-semi": "error",
     "@typescript-eslint/adjacent-overload-signatures": "off",
     // #175
@@ -39,11 +37,12 @@ const tsConfig = tseslint.config({
     "@typescript-eslint/class-literal-property-style": "off",
     "@typescript-eslint/consistent-generic-constructors": "off",
     "@typescript-eslint/consistent-indexed-object-style": "off",
-    "@typescript-eslint/consistent-type-assertions": "off",
+    "@typescript-eslint/consistent-type-assertions": "error",
     "@typescript-eslint/consistent-type-definitions": "off",
-    "@typescript-eslint/consistent-type-imports": ["error"],
+    "@typescript-eslint/consistent-type-exports": "error",
+    "@typescript-eslint/consistent-type-imports": "error",
     "@typescript-eslint/no-confusing-non-null-assertion": "off",
-    // v6 で recommended に追加されたルールを無効化
+    "@typescript-eslint/no-deprecated": "warn",
     "@typescript-eslint/no-duplicate-enum-values": "off",
     "@typescript-eslint/no-import-type-side-effects": "error",
     // this is for react-hook-form
@@ -55,11 +54,11 @@ const tsConfig = tseslint.config({
         },
       },
     ],
-    // v6 で strict に移動したルールを有効化
     "@typescript-eslint/no-non-null-assertion": "warn",
     "@typescript-eslint/no-unsafe-declaration-merging": "off",
     "@typescript-eslint/prefer-for-of": "off",
     "@typescript-eslint/prefer-function-type": "off",
+    "@typescript-eslint/promise-function-async": "error",
     // #97
     "@typescript-eslint/strict-boolean-expressions": [
       "error",
