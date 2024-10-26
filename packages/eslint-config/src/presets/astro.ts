2a09473e5706313af1d102634bfdad8117af14d7
sushichan044 <mail@sushichan.live>
2024-10-26 18:08:03 +0900

    fix: astro config works correctly

--- packages/eslint-config/src/presets/astro.ts
+++ packages/eslint-config/src/presets/astro.ts
@@ -1,13 +1,8 @@
-import type { FlatConfig } from "@typescript-eslint/utils/ts-eslint";
-
 import tseslint from "typescript-eslint";
 
 import { astroConfig } from "../base/astro";
 import react from "./react";
 
-const astro = tseslint.config(
-  ...react,
-  ...astroConfig,
-) satisfies FlatConfig.ConfigArray;
+const astro = tseslint.config(...react, ...astroConfig);
 
 export default astro;
