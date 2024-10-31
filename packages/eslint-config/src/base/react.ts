122a6eb0ecd63b1efc2a8a840a2a52c084b2b510
sushichan044 <mail@sushichan.live>
2024-11-01 04:02:51 +0900

    feat: use eslint-plugin-react-refresh

--- packages/eslint-config/src/base/react.ts
+++ packages/eslint-config/src/base/react.ts
@@ -1,4 +1,6 @@
 import react from "eslint-plugin-react";
+// @ts-expect-error eslint-plugin-react-refresh is not typed
+import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
@@ -20,6 +22,10 @@ const reactConfig = tseslint.config({
     },
   },
   name: "@virtual-live-lab/eslint-config/react",
+  plugins: {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    "react-refresh": reactRefresh,
+  },
   rules: {
     "react/button-has-type": "error",
     "react/iframe-missing-sandbox": "error",
@@ -28,6 +34,9 @@ const reactConfig = tseslint.config({
     "react/jsx-no-target-blank": ["error", { allowReferrer: true }],
     "react/prop-types": "off",
     "react/self-closing-comp": "error",
+    // TODO: 引数から option を受け取る api に変わったら, allowexportnames を設定できるようにする
+    // ref: https://github.com/ArnaudBarre/eslint-plugin-react-refresh?tab=readme-ov-file#allowexportnames-v044
+    "react-refresh/only-export-components": "warn",
   },
   settings: {
     react: {
