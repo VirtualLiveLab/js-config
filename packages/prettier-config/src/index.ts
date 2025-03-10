6d05b3394a8fe410988b2656ac1bb7589abffe32
sushichan044 <mail@sushichan.live>
2025-03-11 01:31:25 +0900

    fix: do not add trailing comma in json-like files

--- packages/prettier-config/src/index.ts
+++ packages/prettier-config/src/index.ts
@@ -4,6 +4,14 @@ const config: Config = {
   arrowParens: "always",
   endOfLine: "lf",
   jsxSingleQuote: false,
+  overrides: [
+    {
+      files: ["*.json", "*.json5", "*.jsonc"],
+      options: {
+        trailingComma: "none",
+      },
+    },
+  ],
   quoteProps: "consistent",
   semi: true,
   singleQuote: false,
