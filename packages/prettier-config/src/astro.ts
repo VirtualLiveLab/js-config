0907959f2a3bcf3dd439ea1c25de315fe01f849e
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-03-21 02:32:34 +0900

    fix(prettier-config): use defu for config merge

--- packages/prettier-config/src/astro.ts
+++ packages/prettier-config/src/astro.ts
@@ -1,18 +1,22 @@
 import type { Config } from "prettier";
 
+import defu from "defu";
+
 import defaultConfig from "./index";
 
-const config: Config = {
-  ...defaultConfig,
-  overrides: [
-    {
-      files: "*.astro",
-      options: {
-        parser: "astro",
+const config: Config = defu(
+  {
+    overrides: [
+      {
+        files: "*.astro",
+        options: {
+          parser: "astro",
+        },
       },
-    },
-  ],
-  plugins: ["prettier-plugin-astro"],
-};
+    ],
+    plugins: ["prettier-plugin-astro"],
+  },
+  defaultConfig,
+);
 
 export default config;
