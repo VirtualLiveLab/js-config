85a0e06f371d429ac36b69556219a0a59f58fc7d
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-03-21 02:36:29 +0900

    fix(stylelint-config): use defu for config merge

--- packages/stylelint-config/src/util.ts
+++ packages/stylelint-config/src/util.ts
@@ -1,24 +1,10 @@
 import type { Config } from "stylelint";
 
-import { mergeWith, uniq } from "es-toolkit";
+import defu from "defu";
 
-export const mergeConfigs = (...configs: Config[]): Config => {
-  return configs.reduce(
-    (acc, config) =>
-      mergeWith(acc, config, (targetValue, sourceValue, key) => {
-        // extends や plugin は deep merge したい
-        if (Array.isArray(targetValue) && Array.isArray(sourceValue)) {
-          // files は deep merge したくない
-          if (key === "files") {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-            return sourceValue;
-          }
-
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-          return uniq(targetValue.concat(sourceValue));
-        }
-        return undefined;
-      }),
-    {},
-  );
+export const mergeConfigs = (
+  topConfig: Config,
+  ...configs: Config[]
+): Config => {
+  return defu(topConfig, ...configs);
 };
