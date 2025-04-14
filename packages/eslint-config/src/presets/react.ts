1cdc8ffd9dccf135a294238076eb8ff304bdf35a
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-04-14 23:02:24 +0900

    fix: remove a11y addon from preset

--- packages/eslint-config/src/presets/react.ts
+++ packages/eslint-config/src/presets/react.ts
@@ -1,9 +1,8 @@
 import tseslint from "typescript-eslint";
 
-import jsxA11y from "../addons/jsxA11y";
 import { reactConfig } from "../base/react";
 import ts from "./ts";
 
-const react = tseslint.config(...ts, ...reactConfig, ...jsxA11y);
+const react = tseslint.config(...ts, ...reactConfig);
 
 export default react;
